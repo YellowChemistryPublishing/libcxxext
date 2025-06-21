b049de60ce232b866517eafeddf41a9d9349c7c8
Bean Can <140042296+YellowChemistryPublishing@users.noreply.github.com>
2025-06-21 18:14:48 +1000

    Fix code that really should not have compiled.

--- sys.Threading/platform/rt_threading.cpp
+++ sys.Threading/platform/rt_threading.cpp
@@ -8,16 +8,16 @@ using namespace sys::platform;
 
 std::atomic<ThreadPool*> ThreadPool::instance;
 
-extern "C" void* _task_operator_new(size_t sz)
+extern "C" void* ::sys::platform::_task_operator_new(size_t sz)
 {
     return ::operator new(sz);
 }
-extern "C" void _task_operator_delete(void* ptr)
+extern "C" void ::sys::platform::_task_operator_delete(void* ptr)
 {
     ::operator delete(ptr);
 }
 
-extern "C" void _launch_async(void* addr)
+extern "C" void ::sys::platform::_launch_async(void* addr)
 {
     if (auto* threadPool = ThreadPool::instance.load())
         threadPool->queue.enqueue(std::coroutine_handle<>::from_address(addr));
