ee50d5b7edd6c4abfe6d77da89ce2f125ee2a20d
Bean Can <140042296+YellowChemistryPublishing@users.noreply.github.com>
2025-06-25 01:20:06 +1000

    Add quaternions.

--- sys.Mathematics/Vector.h
+++ sys.Mathematics/Vector.h
@@ -283,24 +283,6 @@ namespace sys::math
     using vector3i32 = vector3_of<i32>;
 
     using vector4 = vector4_of<>;
-
-    struct matrix3x3u8
-    {
-        u8 m00, m01, m02;
-        u8 m10, m11, m12;
-        u8 m20, m21, m22;
-
-        constexpr bool operator==(const matrix3x3u8& rhs) const noexcept = default;
-    };
-    struct matrix4x4
-    {
-        float m00, m01, m02, m03;
-        float m10, m11, m12, m13;
-        float m20, m21, m22, m23;
-        float m30, m31, m32, m33;
-
-        constexpr bool operator==(const matrix4x4& rhs) const noexcept = default;
-    };
 } // namespace sys::math
 
 namespace sysm = sys::math;
