849fd9903526afb42df73ada4ab579b8a46d3557
Bean Can <140042296+YellowChemistryPublishing@users.noreply.github.com>
2025-06-25 01:11:06 +1000

    Add matrix4x4 type.

--- sys.Mathematics/Vector.h
+++ sys.Mathematics/Vector.h
@@ -292,6 +292,15 @@ namespace sys::math
 
         constexpr bool operator==(const matrix3x3u8& rhs) const noexcept = default;
     };
+    struct matrix4x4
+    {
+        float m00, m01, m02, m03;
+        float m10, m11, m12, m13;
+        float m20, m21, m22, m23;
+        float m30, m31, m32, m33;
+
+        constexpr bool operator==(const matrix4x4& rhs) const noexcept = default;
+    };
 } // namespace sys::math
 
 namespace sysm = sys::math;
