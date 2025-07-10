1092dea27577da4f381a5653721a81fad78e224a
Bean Can <140042296+YellowChemistryPublishing@users.noreply.github.com>
2025-07-10 11:55:57 +1000

    Force `_fence_value_...` to be the unhappy path.

--- sys/LanguageSupport.h
+++ sys/LanguageSupport.h
@@ -83,14 +83,16 @@ struct unsafe
     while (false)
 
 /// @def _fence_value_return(val, retCond)
-/// @brief Return `val` if `retCond` is true.
+/// @brief Return `val` if `retCond` is `true`.
+/// @note Returning must not be the happy path.
 #define _fence_value_return(val, retCond) \
-    if (retCond)                          \
+    if (retCond) [[unlikely]]             \
         return val;
 /// @def _fence_value_co_return(val, retCond)
-/// @brief Coroutine-return `val` if `retCond` is true.
+/// @brief Coroutine-return `val` if `retCond` is `true`.
+/// @note Returning must not be the happy path.
 #define _fence_value_co_return(val, retCond) \
-    if (retCond)                             \
+    if (retCond) [[unlikely]]                \
         co_return val;
 /// @def _fence_contract_enforce(cond)
 /// @brief Enforce a contract, throwing a `contract_violation_exception` if `cond` is false.
