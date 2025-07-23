8bc97288fe31a83021cac88898a000cb2fa77a41
Bean Can <140042296+YellowChemistryPublishing@users.noreply.github.com>
2025-07-23 10:38:52 +1000

    Make `aligned_storage` data easier to access.

--- sys/AlignedStorage.h
+++ sys/AlignedStorage.h
@@ -1,14 +1,22 @@
 #pragma once
 
 #include <algorithm>
+#include <tuple>
 
 #include <LanguageSupport.h>
 
 namespace sys
 {
     template <typename... Ts>
+    requires (sizeof...(Ts) > 0)
     struct aligned_storage
     {
         alignas(Ts...) byte mem[std::max({ sizeof(Ts)... })];
+
+        template <typename T = std::tuple_element<0, std::tuple<Ts...>>::type>
+        T* data()
+        {
+            return _asr(T*, std::addressof(this->mem));
+        }
     };
 } // namespace sys
