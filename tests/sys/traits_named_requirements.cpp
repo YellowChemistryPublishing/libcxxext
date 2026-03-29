d418a4778a65aef5f8ae2810742cc67aa24fe76a
Bean Can <140042296+YellowChemistryPublishing@users.noreply.github.com>
2026-03-30 00:09:38 +1100

    fix: Need an actual test in `traits_named_requirements.cpp`.

--- tests/sys/traits_named_requirements.cpp
+++ tests/sys/traits_named_requirements.cpp
@@ -1,9 +1,17 @@
 #include <memory>
 
+// NOLINTBEGIN(bugprone-throwing-static-initialization, misc-include-cleaner)
+
+#include <catch2/catch_all.hpp>
+
 #include <module/sys>
+
+TEST_CASE("Invariably passes.", "[sys][traits][concepts]") { CHECK(true); }
 
 static_assert(sys::INullablePointer<void*>);
 #if _libcxxext_compiler_msvc // Because this doesn't actually satisfy `sys::INothrowSwappable` on libstdc++.
 static_assert(sys::INullablePointer<std::exception_ptr>);
 #endif
 static_assert(sys::INullablePointer<std::unique_ptr<int>::pointer>);
+
+// NOLINTEND(bugprone-throwing-static-initialization, misc-include-cleaner)
